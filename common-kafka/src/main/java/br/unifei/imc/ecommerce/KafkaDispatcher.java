6be201a8532f8ff616df6f1da34546c7f5ea6629
MatMB115 <MatMB115@users.noreply.github.com>
2023-04-13 17:42:41 -0300

    service update

--- common-kafka/src/main/java/br/unifei/imc/ecommerce/KafkaDispatcher.java
+++ common-kafka/src/main/java/br/unifei/imc/ecommerce/KafkaDispatcher.java
@@ -20,7 +20,7 @@ class KafkaDispatcher<T> implements Closeable {
 
     private static Properties properties() {
         var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "200.235.82.98:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
         return properties;
